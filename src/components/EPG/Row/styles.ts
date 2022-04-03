225ed1f3674fc454febe1136845f21b59292cf76
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2022-04-03 19:48:36 +0200

    :lipstick: Event item height

--- src/components/EPG/Row/styles.ts
+++ src/components/EPG/Row/styles.ts
@@ -1,8 +1,11 @@
 import { StyleSheet } from 'react-native';
+import { dynamicPixels } from 'utils/styles';
 
 export default StyleSheet.create({
   container: {
     flexDirection: 'row',
     overflow: 'hidden',
+    height: dynamicPixels(60),
+    marginVertical: dynamicPixels(1),
   },
 });
