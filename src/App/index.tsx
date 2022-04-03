6fc5d976d0005aab1cd02bb1de44c1bb1375f54c
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2022-04-03 19:48:36 +0200

    :wrench: DISABLE_YELLOW_BOX flag

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -3,11 +3,17 @@ import { withNavigation } from 'hocs/withNavigation';
 import { withSafeAreaContext } from 'hocs/withSafeAreaContext';
 import ms from 'ms';
 import React, { useEffect, useRef, useState } from 'react';
+import { LogBox } from 'react-native';
+import Config from 'react-native-config';
 import { useApp } from 'store/app/hooks';
 import { withStore } from 'store/hocs';
 
 import Navigation from './Navigation';
 import Screen from './Screen';
+
+if (Config.DISABLE_YELLOW_BOX) {
+  LogBox.ignoreAllLogs();
+}
 
 const App = () => {
   const mountTime = useRef<Date>();
