b08eee6f6d4dc9bda34fbdc870770f7cb2c710ca
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2022-04-03 19:48:36 +0200

    :lipstick: Style events a little

--- src/components/EPG/Row/Cell/index.tsx
+++ src/components/EPG/Row/Cell/index.tsx
@@ -1,18 +1,30 @@
-import React, { memo } from 'react';
+import { differenceInMinutes, lightFormat } from 'date-fns';
+import React, { memo, useMemo } from 'react';
 import { Text, View } from 'react-native';
 import { Event } from 'store/events/types';
 
-import styles from './styles';
+import createStyles from './styles';
 
 interface Props {
   event: Event;
 }
 
 const EPGCell = ({ event }: Props) => {
+  const duration = useMemo(
+    () => differenceInMinutes(event.endTime, event.startTime),
+    [event.endTime, event.startTime],
+  );
+
+  const styles = useMemo(() => createStyles({ duration }), [duration]);
+
   return (
     <View style={styles.container}>
       <Text style={styles.title} numberOfLines={1}>
         {event.title}
+      </Text>
+      <Text style={styles.time} numberOfLines={1}>
+        {lightFormat(event.startTime, 'HH:mm')} -{' '}
+        {lightFormat(event.endTime, 'HH:mm')}
       </Text>
     </View>
   );
--- src/components/EPG/Row/Cell/styles.ts
+++ src/components/EPG/Row/Cell/styles.ts
@@ -1,14 +1,26 @@
 import { StyleSheet } from 'react-native';
 
-export default StyleSheet.create({
-  container: {
-    height: '100%',
-    backgroundColor: '#fff',
-    marginHorizontal: 1,
-    padding: 8,
-  },
-  title: {
-    fontWeight: 'bold',
-    fontSize: 16,
-  },
-});
+interface Props {
+  duration: number;
+}
+
+export default ({ duration }: Props) => {
+  return StyleSheet.create({
+    container: {
+      height: '100%',
+      backgroundColor: '#fff',
+      marginHorizontal: 1,
+      padding: 8,
+      width: duration * 4,
+    },
+    title: {
+      fontWeight: 'bold',
+      fontSize: 16,
+    },
+    time: {
+      fontWeight: 'bold',
+      fontSize: 14,
+      opacity: 0.4,
+    },
+  });
+};
