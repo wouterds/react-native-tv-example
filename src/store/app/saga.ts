ca1601ea7359eb57bd2a652a6e88c57fe6d03b73
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2022-04-03 19:48:36 +0200

    :sparkles: Parse channels & events

--- src/store/app/saga.ts
+++ src/store/app/saga.ts
@@ -1,15 +1,113 @@
+import axios from 'axios';
 import { differenceInMilliseconds } from 'date-fns';
-import ms from 'ms';
-import { delay, put, takeEvery } from 'redux-saga/effects';
+import { XMLParser } from 'fast-xml-parser';
+import Config from 'react-native-config';
+import { call, put, takeEvery } from 'redux-saga/effects';
 
-import { bootstrap, bootstrapSuccess } from './slice';
+import { bootstrap, bootstrapError, bootstrapSuccess } from './slice';
+
+const parseChannel = (channel: {
+  '@_id': string;
+  'display-name': string;
+  icon: { '@_src': string };
+}) => ({
+  id: channel?.['@_id'],
+  name: channel?.['display-name'],
+  imageUrl: channel?.icon?.['@_src'],
+});
+
+const parseEventDate = (date: string) => {
+  const year = date.substring(0, 4);
+  const month = date.substring(4, 6);
+  const day = date.substring(6, 8);
+  const hour = date.substring(8, 10);
+  const minute = date.substring(10, 12);
+  const seconds = date.substring(12, 14);
+
+  return new Date(
+    parseInt(year, 10),
+    parseInt(month, 10) - 1,
+    parseInt(day, 10),
+    parseInt(hour, 10),
+    parseInt(minute, 10),
+    parseInt(seconds, 10),
+  );
+};
+
+const parseEvent = (event: {
+  '@_channel': string;
+  '@_start': string;
+  '@_stop': string;
+  desc: { '#text': string };
+  category: Array<{ '#text': string }>;
+  title: { '#text': string };
+}) => {
+  const categories: string[] = [];
+
+  if (Array.isArray(event?.category)) {
+    categories.push(...(event?.category).map(category => category?.['#text']));
+  } else if (typeof event?.category === 'object') {
+    categories.push(event?.category?.['#text']);
+  }
+
+  return {
+    title: event?.title?.['#text'],
+    channelId: event?.['@_channel'],
+    startTime: parseEventDate(event?.['@_start']),
+    endTime: parseEventDate(event?.['@_stop']),
+    description: event?.desc?.['#text'] || null,
+    categories,
+  };
+};
 
 function* bootstrapFlow() {
   const start = new Date();
 
   console.log('[bootstrap] bootstrapping app');
 
-  yield delay(ms('1s'));
+  let data: string;
+  try {
+    ({ data } = yield call(axios.get, Config.EPG_ENDPOINT));
+  } catch (e) {
+    console.error('fetching data failed', e);
+    yield put(bootstrapError());
+    return;
+  }
+
+  if (!data) {
+    console.error('no data');
+    yield put(bootstrapError());
+    return;
+  }
+
+  let channels;
+  let events;
+  try {
+    const parser = new XMLParser({ ignoreAttributes: false });
+    const json = parser.parse(data);
+
+    channels = (json.tv.channel || [])?.map(parseChannel);
+    events = (json.tv.programme || [])?.map(parseEvent);
+  } catch (e) {
+    console.error('parsing data failed', e);
+    yield put(bootstrapError());
+    return;
+  }
+
+  if (!channels?.length) {
+    console.error('no channels');
+    yield put(bootstrapError());
+    return;
+  }
+
+  if (!events?.length) {
+    console.error('no events');
+    yield put(bootstrapError());
+    return;
+  }
+
+  console.log(channels, events);
+
   yield put(bootstrapSuccess());
 
   console.log(
