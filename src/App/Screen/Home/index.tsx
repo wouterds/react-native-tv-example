20cc9b38e02c9eecf8eaf425abce5cf70e8a1a2c
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2022-04-03 19:48:36 +0200

    :sparkles: Or if tablet

--- src/App/Screen/Home/index.tsx
+++ src/App/Screen/Home/index.tsx
@@ -1,11 +1,12 @@
 import EPG from 'components/EPG';
 import React, { memo } from 'react';
 import { Platform, SafeAreaView } from 'react-native';
+import DeviceInfo from 'react-native-device-info';
 
 import styles from './styles';
 
 const HomeScreen = () => {
-  if (Platform.isTV) {
+  if (Platform.isTV || DeviceInfo.isTablet()) {
     return <EPG />;
   }
 
