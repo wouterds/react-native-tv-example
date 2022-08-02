1540285b9227e0c60826445e1d45c467f22c027e
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2022-08-02 16:02:22 +0200

    :wrench: Disable console.log stripping

--- babel.config.js
+++ babel.config.js
@@ -4,7 +4,7 @@ module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
   env: {
     production: {
-      plugins: ['transform-remove-console'],
+      // plugins: ['transform-remove-console'],
     },
   },
   plugins: [
