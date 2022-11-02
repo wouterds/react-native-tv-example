7ee7bbb5eb3ab9ecf3332339635fc7cb5c7986c5
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2022-11-02 16:15:48 +0100

    :art: Simplify component

--- src/components/Swimlane/PopularMovies/index.tsx
+++ src/components/Swimlane/PopularMovies/index.tsx
@@ -1,15 +1,10 @@
 import React, { memo } from 'react';
 import { FlatList, Text, View } from 'react-native';
 import { usePopularMovies } from 'store/popular-movies/hooks';
-import { Movie } from 'store/types/movie';
 import { horizontalSpacing } from 'utils/styles';
 
 import Item from './Item';
 import styles from './styles';
-
-const renderItem = ({ item, index }: { item: Movie | null; index: number }) => {
-  return <Item item={item} index={index} />;
-};
 
 const PopularMoviesSwimlane = () => {
   const { popularMovies } = usePopularMovies();
@@ -18,13 +13,13 @@ const PopularMoviesSwimlane = () => {
     <View style={styles.container}>
       <Text style={[horizontalSpacing, styles.title]}>Popular Movies</Text>
       <FlatList
-        data={popularMovies}
-        renderItem={renderItem}
         contentContainerStyle={[horizontalSpacing, styles.flatList]}
         horizontal={true}
-        keyExtractor={({ id }) => `swimlane.popular-movies.${id}`}
         showsHorizontalScrollIndicator={false}
         showsVerticalScrollIndicator={false}
+        data={popularMovies}
+        keyExtractor={({ id }) => `swimlane.popular-movies.${id}`}
+        renderItem={({ item, index }) => <Item item={item} index={index} />}
       />
     </View>
   );
--- src/components/Swimlane/PopularTVShows/index.tsx
+++ src/components/Swimlane/PopularTVShows/index.tsx
@@ -1,15 +1,10 @@
 import React, { memo } from 'react';
 import { FlatList, Text, View } from 'react-native';
 import { usePopularTVShows } from 'store/popular-tv-shows/hooks';
-import { Show } from 'store/types/show';
 import { horizontalSpacing } from 'utils/styles';
 
 import Item from './Item';
 import styles from './styles';
-
-const renderItem = ({ item, index }: { item: Show | null; index: number }) => {
-  return <Item item={item} index={index} />;
-};
 
 const PopularTVShowsSwimlane = () => {
   const { popularTVShows } = usePopularTVShows();
@@ -18,13 +13,13 @@ const PopularTVShowsSwimlane = () => {
     <View style={styles.container}>
       <Text style={[horizontalSpacing, styles.title]}>Popular TV Shows</Text>
       <FlatList
-        data={popularTVShows}
-        renderItem={renderItem}
         contentContainerStyle={[horizontalSpacing, styles.flatList]}
         horizontal={true}
-        keyExtractor={({ id }) => `swimlane.popular-tv-shows.${id}`}
         showsHorizontalScrollIndicator={false}
         showsVerticalScrollIndicator={false}
+        data={popularTVShows}
+        keyExtractor={({ id }) => `swimlane.popular-tv-shows.${id}`}
+        renderItem={({ item, index }) => <Item item={item} index={index} />}
       />
     </View>
   );
