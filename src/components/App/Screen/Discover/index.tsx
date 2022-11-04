8586a34d807f7b58995d5c70537ca5e91f6c679a
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2022-11-04 14:16:55 +0100

    :fire: Don't render title

--- src/components/App/Screen/Discover/index.tsx
+++ src/components/App/Screen/Discover/index.tsx
@@ -1,20 +1,12 @@
 import ScreenWrapper from 'components/ScreenWrapper';
 import Swimlane from 'components/Swimlane';
-import Title from 'components/Title';
 import React, { memo } from 'react';
-import { Platform } from 'react-native';
-import { horizontalSpacing } from 'utils/styles';
-
-import styles from './styles';
 
 const DiscoverScreen = () => {
   console.log('Render discover screen');
 
   return (
     <ScreenWrapper>
-      {Platform.isTV && (
-        <Title style={[horizontalSpacing, styles.title]}>Discover</Title>
-      )}
       <Swimlane.PopularMovies />
       <Swimlane.PopularTVShows />
     </ScreenWrapper>
