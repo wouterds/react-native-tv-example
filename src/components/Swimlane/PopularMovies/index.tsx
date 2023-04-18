a774f9c6eed60710c1de62b7f94e64033f4f61f8
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2023-04-18 13:42:28 +0200

    refactor(focus): wrap directly with focus guide view

--- src/components/Swimlane/PopularMovies/index.tsx
+++ src/components/Swimlane/PopularMovies/index.tsx
@@ -1,5 +1,5 @@
 import React, { memo } from 'react';
-import { FlatList, Text, TVFocusGuideView, View } from 'react-native';
+import { FlatList, Text, TVFocusGuideView } from 'react-native';
 import { usePopularMovies } from 'store/popular-movies/hooks';
 
 import Item from './Item';
@@ -14,25 +14,23 @@ const PopularMoviesSwimlane = ({ hideTitle, initialFocus }: Props) => {
   const { popularMovies } = usePopularMovies();
 
   return (
-    <TVFocusGuideView trapFocusLeft trapFocusRight>
-      <View style={styles.container}>
-        {!hideTitle && <Text style={styles.title}>Movies</Text>}
-        <FlatList
-          contentContainerStyle={styles.flatList}
-          horizontal={true}
-          showsHorizontalScrollIndicator={false}
-          showsVerticalScrollIndicator={false}
-          data={popularMovies}
-          keyExtractor={({ id }) => `swimlane.popular-movies.${id}`}
-          renderItem={({ item, index }) => (
-            <Item
-              // when initialFocus is true, we want to focus the first item in the list
-              hasTVPreferredFocus={index === 0 && initialFocus}
-              item={item}
-            />
-          )}
-        />
-      </View>
+    <TVFocusGuideView trapFocusLeft trapFocusRight style={styles.container}>
+      {!hideTitle && <Text style={styles.title}>Movies</Text>}
+      <FlatList
+        contentContainerStyle={styles.flatList}
+        horizontal={true}
+        showsHorizontalScrollIndicator={false}
+        showsVerticalScrollIndicator={false}
+        data={popularMovies}
+        keyExtractor={({ id }) => `swimlane.popular-movies.${id}`}
+        renderItem={({ item, index }) => (
+          <Item
+            // when initialFocus is true, we want to focus the first item in the list
+            hasTVPreferredFocus={index === 0 && initialFocus}
+            item={item}
+          />
+        )}
+      />
     </TVFocusGuideView>
   );
 };
--- src/components/Swimlane/PopularTVShows/index.tsx
+++ src/components/Swimlane/PopularTVShows/index.tsx
@@ -1,5 +1,5 @@
 import React, { memo } from 'react';
-import { FlatList, Text, TVFocusGuideView, View } from 'react-native';
+import { FlatList, Text, TVFocusGuideView } from 'react-native';
 import { usePopularTVShows } from 'store/popular-tv-shows/hooks';
 
 import Item from './Item';
@@ -14,25 +14,23 @@ const PopularTVShowsSwimlane = ({ hideTitle, initialFocus }: Props) => {
   const { popularTVShows } = usePopularTVShows();
 
   return (
-    <TVFocusGuideView trapFocusLeft trapFocusRight>
-      <View style={styles.container}>
-        {!hideTitle && <Text style={styles.title}>Shows</Text>}
-        <FlatList
-          contentContainerStyle={styles.flatList}
-          horizontal={true}
-          showsHorizontalScrollIndicator={false}
-          showsVerticalScrollIndicator={false}
-          data={popularTVShows}
-          keyExtractor={({ id }) => `swimlane.popular-tv-shows.${id}`}
-          renderItem={({ item, index }) => (
-            <Item
-              // when initialFocus is true, we want to focus the first item in the list
-              hasTVPreferredFocus={index === 0 && initialFocus}
-              item={item}
-            />
-          )}
-        />
-      </View>
+    <TVFocusGuideView trapFocusLeft trapFocusRight style={styles.container}>
+      {!hideTitle && <Text style={styles.title}>Shows</Text>}
+      <FlatList
+        contentContainerStyle={styles.flatList}
+        horizontal={true}
+        showsHorizontalScrollIndicator={false}
+        showsVerticalScrollIndicator={false}
+        data={popularTVShows}
+        keyExtractor={({ id }) => `swimlane.popular-tv-shows.${id}`}
+        renderItem={({ item, index }) => (
+          <Item
+            // when initialFocus is true, we want to focus the first item in the list
+            hasTVPreferredFocus={index === 0 && initialFocus}
+            item={item}
+          />
+        )}
+      />
     </TVFocusGuideView>
   );
 };
