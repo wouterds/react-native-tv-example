7b2a1b26e566096ee64eb19a91766d8b7e26c619
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2023-04-18 11:32:15 +0200

    feat(menu-button): only exit app when on root screen on back press

--- src/components/ScreenWrapper/index.tsx
+++ src/components/ScreenWrapper/index.tsx
@@ -1,5 +1,7 @@
-import React, { memo, ReactNode } from 'react';
-import { ScrollView, View, ViewStyle } from 'react-native';
+import { useIsFocused, useRoute } from '@react-navigation/native';
+import { Route } from 'navigation';
+import React, { memo, ReactNode, useEffect } from 'react';
+import { ScrollView, TVEventControl, View, ViewStyle } from 'react-native';
 
 import styles from './styles';
 import TVHeaderNavigation from './TVHeaderNavigation';
@@ -11,6 +13,21 @@ interface Props {
 }
 
 const ScreenWrapper = ({ children, style, header }: Props) => {
+  const isFocused = useIsFocused();
+  const { name: route } = useRoute();
+
+  useEffect(() => {
+    if (isFocused) {
+      if (Route.Discover === route) {
+        // exit on back press
+        TVEventControl.disableTVMenuKey();
+      } else {
+        // navigate back on back press
+        TVEventControl.enableTVMenuKey();
+      }
+    }
+  }, [route, isFocused]);
+
   return (
     <View style={[styles.container, style]}>
       <ScrollView
