f233a91e25102d88db012f2169568a09b65652f7
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2023-04-26 20:29:02 +0200

    feat(videos): link reducer

--- src/store/reducer.ts
+++ src/store/reducer.ts
@@ -4,12 +4,14 @@ import { reducer as app } from './app/slice';
 import { reducer as popularMovies } from './popular-movies/slice';
 import { reducer as popularTVShows } from './popular-tv-shows/slice';
 import { reducer as trendingToday } from './trending-today/slice';
+import { reducer as videos } from './videos/slice';
 
 const reducer = combineReducers({
   app,
   popularTVShows,
   popularMovies,
   trendingToday,
+  videos,
 });
 
 export default reducer;
