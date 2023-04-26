ab4384caa76baefe86819668adf479e05d8977e3
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2023-04-26 20:29:13 +0200

    feat(videos): link sagas

--- src/store/saga.ts
+++ src/store/saga.ts
@@ -4,8 +4,9 @@ import app from './app/saga';
 import popularMovies from './popular-movies/saga';
 import popularTVShows from './popular-tv-shows/saga';
 import trendingToday from './trending-today/saga';
+import videos from './videos/saga';
 
-const sagas = [app, trendingToday, popularTVShows, popularMovies];
+const sagas = [app, trendingToday, popularTVShows, popularMovies, videos];
 
 const saga = function* () {
   yield all(sagas.map(fork));
