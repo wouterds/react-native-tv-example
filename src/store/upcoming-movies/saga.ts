8f9643fbc3733fa4f0161c29cf092b178aa9b984
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2023-05-02 21:42:51 +0200

    feat: only show future movies

--- src/store/upcoming-movies/saga.ts
+++ src/store/upcoming-movies/saga.ts
@@ -2,6 +2,7 @@ import { AxiosResponse } from 'axios';
 import { formatDistanceToNowStrict } from 'date-fns';
 import { call, put, takeEvery } from 'redux-saga/effects';
 import { Api } from 'services/api';
+import { Movie } from 'store/types/movie';
 import { AxiosFactory, Normalizer } from 'store/utils';
 
 import {
@@ -30,7 +31,10 @@ function* fetchUpcomingMoviesFlow() {
     return;
   }
 
-  const data = response.data.results.map(Normalizer.movie);
+  const data = response.data.results
+    .map(Normalizer.movie)
+    .filter((item: Movie) => new Date(item.release_date) > new Date())
+    .filter(Boolean);
 
   yield put(fetchUpcomingMoviesSuccess(data));
 
