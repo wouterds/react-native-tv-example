acb14609239ad48ec7b1ae6aca493a14e6dcaeb1
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2023-07-03 14:21:45 +0200

    feat(movies): should also work with mixed content

--- src/store/movies/saga.ts
+++ src/store/movies/saga.ts
@@ -1,17 +1,26 @@
 import { put, takeEvery } from 'redux-saga/effects';
 import { fetchPopularMoviesSuccess } from 'store/popular-movies/slice';
 import { fetchTopRatedMoviesSuccess } from 'store/top-rated-movies/slice';
+import { fetchTrendingTodaySuccess } from 'store/trending-today/slice';
+import { Movie } from 'store/types/movie';
 import { fetchUpcomingMoviesSuccess } from 'store/upcoming-movies/slice';
 
 import { addMovies } from './slice';
-import { AddMoviesAction } from './types';
+import { AddMixedMoviesAction, AddMoviesAction } from './types';
 
 function* addMoviesFlow({ payload }: AddMoviesAction) {
   yield put(addMovies(payload));
+}
+
+function* addMixedMoviesFlow({ payload }: AddMixedMoviesAction) {
+  yield put(
+    addMovies(payload.filter(item => item.type === 'movie') as Movie[]),
+  );
 }
 
 export default function* watchPopularMovies() {
   yield takeEvery(fetchPopularMoviesSuccess, addMoviesFlow);
   yield takeEvery(fetchUpcomingMoviesSuccess, addMoviesFlow);
   yield takeEvery(fetchTopRatedMoviesSuccess, addMoviesFlow);
+  yield takeEvery(fetchTrendingTodaySuccess, addMixedMoviesFlow);
 }
--- src/store/movies/types.ts
+++ src/store/movies/types.ts
@@ -1,4 +1,5 @@
 import { Movie } from 'store/types/movie';
+import { Show } from 'store/types/show';
 
 type MovieId = number;
 
@@ -7,4 +8,9 @@ export type MoviesState = Record<MovieId, Movie>;
 export interface AddMoviesAction {
   type: string;
   payload: Movie[];
+}
+
+export interface AddMixedMoviesAction {
+  type: string;
+  payload: Array<Movie | Show>;
 }
