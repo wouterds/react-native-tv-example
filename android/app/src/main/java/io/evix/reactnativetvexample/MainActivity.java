874229c5a049a87b65f8e3fa9c51d164b03a3554
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2023-07-04 09:07:50 +0200

    feat: edge-to-edge layout

--- android/app/src/main/java/io/evix/reactnativetvexample/MainActivity.java
+++ android/app/src/main/java/io/evix/reactnativetvexample/MainActivity.java
@@ -1,10 +1,23 @@
 package io.evix.reactnativetvexample;
 
+import android.os.Bundle;
+import android.view.View;
+
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
 
 public class MainActivity extends ReactActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    // https://developer.android.com/develop/ui/views/layout/edge-to-edge
+    getWindow().getDecorView().setSystemUiVisibility(
+      View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+    );
+
+    super.onCreate(savedInstanceState);
+  }
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
